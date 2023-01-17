34a8b7f5ddf03eec81c1caad13678c9437d7a11c
Pardal147 <108307927+Pardal147@users.noreply.github.com>
2023-01-17 11:07:10 -0300

    arrumado o problema

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
 
 let btn = document.getElementById('btn')
+let comentario = document.getElementById('comentario')
+comentario.style.display ='none'
 
 btn.onclick=()=>{
 
     let nome = document.getElementById('nome')
     let altura = document.getElementById('altura')
     let peso = document.getElementById('peso')
-    let comentario = document.getElementById('comentario')
-    let resultado = peso.value/(Math.pow(altura.value,2))
+    let resultado = Math.round(peso.value/Math.pow(altura.value,2))
     let grau;
 
     if(nome.value !== '' && altura.value !== ''&& peso.value !== ''){
         if(resultado < 18.5){
+            comentario.style.display ='block'
             grau = 'Abaixo do peso'
             console.log(nome.value+' seu IMC é '+resultado+'você está '+grau)
             comentario.innerHTML = nome.value+' seu IMC é '+resultado+' você está '+grau
         }
         if(resultado >= 18.5 && resultado < 24.9){
+            comentario.style.display ='block'
             grau = 'Peso normal'
             console.log(nome.value+' seu IMC é '+resultado+'você esta '+grau)
             comentario.innerHTML = nome.value+' seu IMC é '+resultado+' você esta '+grau
         }
         if(resultado >= 25 && resultado < 29.9){
+            comentario.style.display ='block'
             grau = 'Pré-Obesidade'
             console.log(nome.value+' seu IMC é '+resultado+'você esta '+grau)
             comentario.innerHTML = nome.value+' seu IMC é '+resultado+' você esta '+grau
         }
         if(resultado >= 30 && resultado < 34.9){
+            comentario.style.display ='block'
             grau = 'Obesidade Grau 1'
             console.log(nome.value+' seu IMC é '+resultado+'você esta '+grau)
             comentario.innerHTML = nome.value+' seu IMC é '+resultado+' você esta '+grau
         }
         if(resultado >= 35 && resultado <39.9){
+            comentario.style.display ='block'
             grau = 'Obesidade Grau 2'
             console.log(nome.value+' seu IMC é '+resultado+'você esta '+grau)
             comentario.innerHTML = nome.value+' seu IMC é '+resultado+' você esta '+grau 
         }
         if(resultado >=40){
+            comentario.style.display ='block'
             grau = 'Obesidade Grau 3'
             console.log(nome.value+' seu IMC é '+resultado+'você esta '+grau)
             comentario.innerHTML = nome.value+' seu IMC é '+resultado+' você esta '+grau
         }
     }
     else{
+        comentario.style.display ='none'
         // comentario.innerHTML = 'Preencha todos os campos'
         window.alert('Preencha todos os campos')
     }
